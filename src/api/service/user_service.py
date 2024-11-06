f5ffa6c268061e49f027ba17adbb9f10363d632c
FPVInMyScouter <42262645+FPVInMyScouter@users.noreply.github.com>
2024-11-06 23:15:01 +0530

    Revert "[BE] Implement GET /user/{id}"

--- src/api/service/user_service.py
+++
@@ -1,8 +0,0 @@
-from repository.user_repository import UserRepository
-
-class UserService:
-    def __init__(self):
-        self.user_repository = UserRepository()
-
-    def get_user(self, user_id: int):
-        return self.user_repository.get_user_by_id(user_id)
